14186257b83571e36ede838d0d0484ead8ce233b
Alexey Verevkin <lesha.verevkin.02@gmail.com>
2021-11-27 11:36:13 +0300

    fix: clear mongodb URL

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const router = require("./authRouter");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const mongo_URL = "mongodb+srv:/";
+const mongo_URL = "mongodb+srv:";
 
 app.use(express.json());
 app.use("/auth", router);
